77f08c13464f7f49232e4f51e441b7ae68d651a2
vashishthnrayan <vedmedicare123@gmail.com>
2026-05-26 16:25:26 +0530

    opration_on_list

---
+++ opration_on_list.py
@@ -0,0 +1,52 @@
+list_1=["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(list_1)
+
+print(len(list_1))
+
+
+# fist element
+print(list_1[0])
+
+# last element
+print(list_1[-1])
+
+
+# append element
+
+
+list_1.append("grape")
+print(list_1)
+
+# remove element
+
+list_1.remove("banana")
+print(list_1)
+
+# sort list
+
+list_1.sort()
+print(list_1)
+
+
+# pop element
+
+list_1.pop(2)
+print(list_1)
+
+# reserve list
+
+list_1.reverse()
+print(list_1)
+
+# multiply list
+print(list_1*2)
+
+
+# slicing list
+
+print(list_1[1:5])
+
+# clear list
+
+list_1.clear()
+print(list_1)
