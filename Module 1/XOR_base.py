fe889f6ad9405ec1733dcc23d346cf487eaedcef
vashishthnrayan <vedmedicare123@gmail.com>
2026-08-29 16:18:31 +0530

    cor base

---
+++ Module 1/XOR_base.py
@@ -0,0 +1,10 @@
+input("XOR with 0 keeps the same value. Press enter to continue")
+
+print("   5 ^ 0 = ",5^0)
+print("   9 ^ 0 = ",9^0)
+
+n  = int(input("Enter a number (try 6 or 11)"))
+guess = input("What is 3 ^" + str(n) + " ^ 3?")
+
+input("Press enter to see the answer")
+print("   3 ^",n," ^ 3 = ",3^n^3 ,"your guess:",guess)
