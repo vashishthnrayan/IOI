0c0fb007f1e2d70be0cfad8efd28cc0875ce0e5e
vashishthnrayan <vedmedicare123@gmail.com>
2026-09-07 16:32:48 +0530

    shift opp

---
+++ shift opp.py
@@ -0,0 +1,10 @@
+input("Left shift doubl , right shift half : Press Enter to continue ")
+print("   3 << 1  = ",3 << 1,"  12 >> 1 = ",12 >> 1)
+print("   3 << 2  = ",3 << 2,"  12 >> 2 = ",12 >> 2)
+
+
+n = int(input("enter a number (try 5 or 8):"))
+guess = input( " what will " + str(n) + " become after left shifting by 1? : ")
+input ("Left shift by 2 multiplies  by 4 .Press Enter to continue ")
+print(" ",n,"<< 2 =",n << 2,"your guess was : ",guess
+      )
